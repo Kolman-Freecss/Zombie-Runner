a31364d25e06b7fb7fc7b0e894c8522b44014cca
Ragnarok <s3rg10.martinez.roman@gmail.com>
2022-09-11 03:52:30 +0200

    Step 174

--- Assets/Enemy/EnemyAI.cs
+++ Assets/Enemy/EnemyAI.cs
@@ -32,6 +32,11 @@ public class EnemyAI : MonoBehaviour
         }
     }
 
+    public void OnDamageTaken()
+    {
+        isProvoked = true;
+    }
+
     void EngageTarget()
     {
         FaceTarget();
--- Assets/Environment/SceneLoader.cs
+++ Assets/Environment/SceneLoader.cs
@@ -9,6 +9,7 @@ public class SceneLoader : MonoBehaviour
     public void ReloadGame()
     {
         SceneManager.LoadScene(0);
+        Time.timeScale = 1;
     }
     
     public void QuitGame()
